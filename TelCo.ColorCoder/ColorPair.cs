a6aff1b65458865c822b229acf17b48c01e46032
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-02-23 08:52:50 +0530

    updated file

--- TelCo.ColorCoder/ColorPair.cs
+++ TelCo.ColorCoder/ColorPair.cs
@@ -1,27 +1,18 @@
-
 using System;
-using System.Diagnostics;
 using System.Drawing;
 namespace TelCo.ColorCoder
 {
     public  class ColorPair
     {
-        public Color major;
-        public Color minor;
-        public static Color[] MapMajor;
-        public static Color[] MapMinor;
-        public override string ToString()
-        {
-            return string.Format("MajorColor:{0}, MinorColor:{1}", major.Name, minor.Name);
-        }
+        public Color major, minor;
+        public static Color[] MapMajor, MapMinor;
         public static ColorPair GetColorFromPairNumber(int pairNumber)
         {
           int minorSize = MapMinor.Length;
             int majorSize =MapMajor.Length;
             if (pairNumber < 1 || pairNumber > minorSize * majorSize)
             {
-                throw new ArgumentOutOfRangeException(
-                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
+                throw new ArgumentOutOfRangeException(string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
             }
          int zeroBasedPairNumber = pairNumber - 1;
             int majorIndex = zeroBasedPairNumber / minorSize;
@@ -34,17 +25,15 @@ namespace TelCo.ColorCoder
         }
         public static int GetPairNumberFromColor(ColorPair pair)
         {
-         int majorIndex = -1;
             int minorColorIndex = GetColorIndex(pair.major, MapMinor);
             int majorColorIndex = GetColorIndex(pair.minor, MapMajor);
             if (minorColorIndex == -1 || majorColorIndex == -1)
             {
-                throw new ArgumentException(
-                    string.Format("Unknown Colors: {0}", pair.ToString()));
-            } return (majorIndex * MapMinor.Length) + (minorColorIndex + 1);
+                throw new ArgumentException($"Unknown Colors: {pair}");
+            }
+            return (majorColorIndex * MapMinor.Length) + (minorColorIndex + 1);
         }
-
-        public static int GetColorIndex(Color pair, Color[] colorMap)
+  public static int GetColorIndex(Color pair, Color[] colorMap)
         {
             int index = -1;
             for (int i = 0; i < colorMap.Length; i++)
